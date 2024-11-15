5a54d8829e08c3b12d91acbab9d9229ce8e0d387
Justin Ottesen <49543532+justinottesen@users.noreply.github.com>
2024-11-15 11:22:37 -0500

    Set up google test (#21)
    * Set up google test
    
    * Restructure code to separate from deliverables
    
    * Add virtualenv for python development
    
    * Only build googletest if not built
    
    * Update readme to describe virtualenv setup
    
    * Fix .gitignore to not skip virtualenv bin dir

---
+++ Code/setup_repo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+
+REPO_DIR="$(pwd)"
+GTEST_DIR="$REPO_DIR/googletest"
+
+if [ ! -d "$GTEST_DIR" ]; then
+  echo "Cloning googletest repository at $GTEST_DIR"
+  git clone https://github.com/google/googletest.git googletest -b v1.15.2
+else
+  echo "Found googletest at $GTEST_DIR"
+fi
+
+if [ ! -d "$GTEST_DIR/build" ]; then
+  echo "Building googletest"
+  cd "$GTEST_DIR"
+  mkdir -p build
+  cd build
+  cmake ..
+  make
+else
+  echo "Google test already built"
+fi
