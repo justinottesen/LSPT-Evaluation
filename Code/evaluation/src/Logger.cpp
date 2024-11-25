87df65492bc9a88be6ed1a81a83adbb5b39c53c2
Justin Ottesen <justinottesen@gmail.com>
2024-11-25 11:25:44 -0500

    Added logging library

---
+++ Code/evaluation/src/Logger.cpp
@@ -0,0 +1,116 @@
+#include "Logger.h"
+
+#include <cstddef>
+#include <filesystem>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <ostream>
+#include <shared_mutex>
+#include <string>
+#include <string_view>
+
+#include "TimeUtil.h"
+
+static constexpr unsigned int LEVEL_FMT_WIDTH   = 8;
+static constexpr unsigned int TIME_FMT_WIDTH    = 25;
+static constexpr unsigned int FULL_HEADER_WIDTH = 100;
+
+Logger::Logger(LogLevel level, const std::filesystem::path& path, int line, const char* function)
+    : m_level(level) {
+  m_buffer << "[" << std::setw(LEVEL_FMT_WIDTH) << toStr(level) << "] " << path.filename().c_str()
+           << ":" << line << " in " << function << "(): ";
+}
+
+Logger::~Logger() { Workers::log(m_level, m_buffer.str()); }
+
+void Logger::Workers::addWorker(const std::filesystem::path& path, LogLevel level) {
+  const std::lock_guard<std::shared_mutex> lock(mutex());
+  if (path == "") {
+    workers().try_emplace(path, std::make_unique<ConsoleWorker>(level));
+  } else {
+    workers().try_emplace(path, std::make_unique<FileWorker>(level, path));
+  }
+}
+
+void Logger::Workers::setLevel(const std::filesystem::path& path, LogLevel level) {
+  const std::lock_guard<std::shared_mutex> lock(mutex());
+  workers().at(path)->setLevel(level);
+}
+
+void Logger::Workers::removeWorker(const std::filesystem::path& path) {
+  const std::lock_guard<std::shared_mutex> lock(mutex());
+  workers().erase(path);
+}
+
+void Logger::Workers::log(LogLevel level, std::string_view msg) {
+  const std::shared_lock<std::shared_mutex> lock(mutex());
+  for (auto& [_, worker] : workers()) { worker->log(level, msg); }
+}
+
+void Logger::Workers::Worker::log(LogLevel level, std::string_view msg) {
+  const std::lock_guard<std::mutex> lock(m_mutex);
+  if (level > m_level) { return; }
+  logTime();
+  stream() << log_color(level);
+  std::size_t start = 0;
+  while (start < msg.size()) {
+    std::size_t end = msg.find('\n', start);
+    if (end == std::string::npos) { end = msg.size(); }
+    if (start > 0) { stream() << std::string(TIME_FMT_WIDTH + LEVEL_FMT_WIDTH + 3, ' ') << "-> "; }
+    stream() << msg.substr(start, end + 1);
+    start = end + 1;
+  }
+  stream() << reset_color() << '\n';
+}
+
+void Logger::Workers::Worker::setLevel(LogLevel level) {
+  const std::lock_guard<std::mutex> lock(m_mutex);
+  logTime();
+  stream() << "Changing log level from " << toStr(m_level) << " to " << toStr(level) << '\n';
+  m_level = level;
+}
+
+void Logger::Workers::Worker::logTime() { stream() << current_time() << " | "; }
+
+void Logger::Workers::Worker::logOpeningMessage(std::string_view location) {
+  stream() << std::string(TIME_FMT_WIDTH, '-') << '\n';
+  logTime();
+  stream() << std::string(FULL_HEADER_WIDTH - TIME_FMT_WIDTH, '-') << '\n';
+  logTime();
+  stream() << "Opening \"" << location << "\" for logging at level " << toStr(m_level) << '\n';
+}
+
+Logger::Workers::FileWorker::FileWorker(LogLevel level, const std::filesystem::path& path)
+    : Worker(level) {
+  if (!std::filesystem::exists(path) && path.has_parent_path()) {
+    std::filesystem::create_directories(path.parent_path());
+  }
+  m_stream.open(path, std::ios::app);
+  if (!m_stream.good()) { std::cerr << "Unable to open log at " << path << '\n'; }
+  logOpeningMessage(path.string());
+}
+
+std::string_view toStr(LogLevel level) {
+  switch (level) {
+    case CRITICAL: return "CRITICAL";
+    case ERROR:    return "ERROR";
+    case WARN:     return "WARN";
+    case INFO:     return "INFO";
+    case DEBUG:    return "DEBUG";
+    case TRACE:    return "TRACE";
+  }
+}
+
+std::string_view Logger::Workers::ConsoleWorker::log_color(LogLevel level) {
+  switch (level) {
+    case CRITICAL: return "\033[31;1m";
+    case ERROR:    return "\033[31m";
+    case WARN:     return "\033[33m";
+    case INFO:     return "";
+    case DEBUG:    return "\033[2m";
+    case TRACE:    return "\033[2;3m";
+  }
+}
---
+++ Code/evaluation/src/Logger.h
@@ -0,0 +1,132 @@
+#pragma once
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <shared_mutex>
+#include <sstream>
+#include <string>
+
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage): Macro needed for log values
+#define LOG(level) \
+  if (true) Logger(level, __FILE__, __LINE__, __FUNCTION__).stream()
+
+enum LogLevel : char { CRITICAL, ERROR, WARN, INFO, DEBUG, TRACE };
+
+std::string_view toStr(LogLevel level);
+
+class Logger {
+ public:
+  Logger(LogLevel level, const std::filesystem::path& path, int line, const char* function);
+  ~Logger();
+
+  // Delete all other constructors & assignment
+  Logger(const Logger&)             = delete;
+  Logger& operator=(const Logger&)  = delete;
+  Logger(const Logger&&)            = delete;
+  Logger& operator=(const Logger&&) = delete;
+
+  static void addFile(const std::string& path, LogLevel level = INFO) {
+    Workers::addWorker(path, level);
+  }
+
+  static void addConsole(LogLevel level = INFO) { Workers::addWorker("", level); }
+
+  static void setConsoleLevel(LogLevel level) { Workers::setLevel("", level); }
+
+  static void setLevel(const std::filesystem::path& path, LogLevel level) {
+    Workers::setLevel(path, level);
+  }
+
+  static void removeFile(const std::filesystem::path& path) { Workers::removeWorker(path); }
+
+  static void removeConsole() { Workers::removeWorker(""); }
+
+  std::ostream& stream() { return m_buffer; }
+
+ private:
+  LogLevel m_level;
+
+  std::ostringstream m_buffer;
+
+  class Workers {
+   public:
+    static void addWorker(const std::filesystem::path& path, LogLevel level = INFO);
+
+    static void setLevel(const std::filesystem::path& path, LogLevel level);
+
+    static void removeWorker(const std::filesystem::path& path);
+
+    static void log(LogLevel level, std::string_view msg);
+
+   private:
+    class Worker {
+     public:
+      explicit Worker(LogLevel level)
+          : m_level(level) {}
+      virtual ~Worker() = default;
+
+      // Delete all other constructors & assignment
+      Worker(const Worker&)             = delete;
+      Worker& operator=(const Worker&)  = delete;
+      Worker(const Worker&&)            = delete;
+      Worker& operator=(const Worker&&) = delete;
+
+      void log(LogLevel level, std::string_view msg);
+
+      void setLevel(LogLevel level);
+
+     protected:
+      void logTime();
+
+      void logOpeningMessage(std::string_view location);
+
+     private:
+      virtual std::ostream& stream() = 0;
+
+      virtual std::string_view log_color(LogLevel /*unused*/) { return ""; }
+      virtual std::string_view reset_color() { return ""; }
+
+      std::mutex m_mutex;
+      LogLevel   m_level;
+    };
+
+    class ConsoleWorker : public Worker {
+     public:
+      explicit ConsoleWorker(LogLevel level)
+          : Worker(level) {
+        logOpeningMessage("Console");
+      }
+      // ~ConsoleWorker() override = default;
+
+     private:
+      std::ostream& stream() override { return std::cout; }
+
+      std::string_view log_color(LogLevel level) override;
+      std::string_view reset_color() override { return "\033[0m"; }
+    };
+
+    class FileWorker : public Worker {
+     public:
+      FileWorker(LogLevel level, const std::filesystem::path& path);
+      // ~FileWorker() override = default;
+
+     private:
+      std::ostream& stream() override { return m_stream; }
+
+      std::ofstream m_stream;
+    };
+
+    static std::shared_mutex& mutex() {
+      static std::shared_mutex mutex;
+      return mutex;
+    }
+
+    static std::map<std::string, std::unique_ptr<Worker>>& workers() {
+      static std::map<std::string, std::unique_ptr<Worker>> workers;
+      return workers;
+    }
+  };
+};
