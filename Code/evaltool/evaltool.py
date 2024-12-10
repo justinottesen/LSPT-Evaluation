1691b00ea68cae041d78872b8eefbed53d2b443b
Justin Ottesen <49543532+justinottesen@users.noreply.github.com>
2024-12-10 10:11:47 -0500

    Filled out implementation of evaltool.py (#62)
    * Filled out implementation of evaltool.py
    
    * Add required tag to arguments

--- Code/evaltool/evaltool.py
+++ Code/evaltool/evaltool.py
@@ -1,24 +1,83 @@
 #!/usr/bin/python3 
 
 import argparse
+import requests
+from typing import Any
+
+def get_url(ip: str, port: int, path: str):
+  return f"http://{ip}:{port}{path}"
+
+def make_request(ip: str, port: int, method: str, path: str, headers: dict[str, str] = {}, body: Any | None = None) -> None:
+  url = get_url(ip, port, path)
+
+  response = requests.request(method, url, headers=headers, json=body)
+
+  print(response.status_code, response.reason)
+  for h, v in response.headers.items():
+    print(f"{h}: {v}")
+  print()
+  if response.content: print(response.content.decode())
 
 def cmd_get_autofill(args) -> None:
-  raise NotImplementedError
+  ip = args.ip
+  port = args.port
+
+  partial_query = args.partial_query
+  num_suggestinos = args.num_suggestions
+
+  headers = {
+    "partial_query": partial_query,
+    "num_suggestions": str(num_suggestinos)
+  }
+
+  make_request(ip, port, "GET", "/v0/GetAutofill", headers = headers)
 
 def cmd_get_query_id(args) -> None:
-  raise NotImplementedError
+  ip = args.ip
+  port = args.port
+
+  make_request(ip, port, "GET", "/v0/GetQueryID")
 
 def cmd_report_search_results(args) -> None:
-  raise NotImplementedError
+  ip = args.ip
+  port = args.port
+
+  results_json = args.results_json
+
+  make_request(ip, port, "POST", "/v0/ReportSearchResults", body = results_json)
 
 def cmd_submit_feedback(args) -> None:
-  raise NotImplementedError
+  ip = args.ip
+  port = args.port
+
+  feedback_json = args.feedback_json
+
+  make_request(ip, port, "POST", "/v0/SubmitFeedback", body = feedback_json)
 
 def cmd_get_query_data(args) -> None:
-  raise NotImplementedError
+  ip = args.ip
+  port = args.port
+
+  query_ID = args.query_ID
+
+  headers = {
+    "query_ID": str(query_ID)
+  }
+
+  make_request(ip, port, "GET", "/v0/GetQueryData", headers = headers)
 
 def cmd_report_metrics(args) -> None:
-  raise NotImplementedError
+  ip = args.ip
+  port = args.port
+
+  component = args.component
+  metrics_json = args.metrics_json
+
+  headers = {
+    "component": component
+  }
+
+  make_request(ip, port, "POST", "/v0/SubmitFeedback", headers = headers, body = metrics_json)
 
 def cmd_proxy(args) -> None:
   raise NotImplementedError
@@ -28,8 +87,8 @@ if __name__ == "__main__":
         description="An admin tool for testing and interfacing with the evaluation component"
     )
   
-  parser.add_argument("--ip", type=str, default="127.0.0.1", help="The IP the evaluation component is running on")
-  parser.add_argument("--port", "-p", type=int, default=8080, help="The port the evaluation component is running on")
+  parser.add_argument("--ip", type=str, default="127.0.0.1", required=True, help="The IP the evaluation component is running on")
+  parser.add_argument("--port", "-p", type=int, default=8080, required=True, help="The port the evaluation component is running on")
 
   subparsers = parser.add_subparsers(
     title="commands",
@@ -41,31 +100,31 @@ if __name__ == "__main__":
   # GetAutofill
   parser_autofill = subparsers.add_parser("GetAutofill", help = "Get autofill results for a partial query")
   parser_autofill.add_argument("partial_query", type=str, help="A partial query to generate autofill for")
-  parser_autofill.add_argument("num_suggestions", type=str, help="The maximum number of responses wanted")
+  parser_autofill.add_argument("num_suggestions", type=int, help="The maximum number of responses wanted")
 
   # GetQueryID
   parser_getQueryID = subparsers.add_parser("GetQueryID", help = "Gets a unique query ID")
   
   # ReportSearchResults
   parser_reportSearchResults = subparsers.add_parser("ReportSearchResults", help="Report search results to the database")
-  parser_reportSearchResults.add_argument("results_json", type=str, help = "The JSON body including all necessary information")
+  parser_reportSearchResults.add_argument("results_json", type=str, required=True, help = "The JSON body including all necessary information")
 
   # SubmitFeedback
   parser_submitFeedback = subparsers.add_parser("SubmitFeedback", help = "Submit user feedback to the component")
-  parser_submitFeedback.add_argument("feedback_json", type=str, help = "The JSON body including all necessary information")
+  parser_submitFeedback.add_argument("feedback_json", type=str, required=True, help = "The JSON body including all necessary information")
 
   # GetQeuryData
   parser_getQueryData = subparsers.add_parser("GetQueryData", help = "Gets the data associated with a query ID")
-  parser_getQueryData.add_argument("query_ID", type=int, help="The unique identifier of the query of interest")
+  parser_getQueryData.add_argument("query_ID", type=int, required=True, help="The unique identifier of the query of interest")
 
   # ReportMetrics
   parser_reportMetrics = subparsers.add_parser("ReportMetrics", help="Submit component metrics to the datastore")
-  parser_reportMetrics.add_argument("component", type=str, help="The name of the component submitting metrics")
-  parser_reportMetrics.add_argument("metrics_json", type=str, help = "The JSON body including all necessary information")
+  parser_reportMetrics.add_argument("component", required=True, type=str, help="The name of the component submitting metrics")
+  parser_reportMetrics.add_argument("metrics_json", required=True, type=str, help = "The JSON body including all necessary information")
 
   # Proxy
   parser_proxy = subparsers.add_parser("proxy", help = "Intercept, print, and relay all messages sent to the component")
-  parser_proxy.add_argument("proxy_port", type=int, help="Which port to listen for messages on")
+  parser_proxy.add_argument("proxy_port", type=int, required=True, help="Which port to listen for messages on")
 
   args = parser.parse_args()
 
@@ -79,4 +138,7 @@ if __name__ == "__main__":
     "proxy": cmd_proxy
   }
 
-  command_handler[args.command](args)
+  if args.command in command_handler:
+    command_handler[args.command](args)
+  else:
+    parser.print_help()
